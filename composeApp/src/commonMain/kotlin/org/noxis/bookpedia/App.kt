bcc03e6110d187a72311f270d451d10cd855116f
Tumanov Vycheslav <63115274+NoXis82@users.noreply.github.com>
2025-02-25 22:17:11 +0300

    Koin setup

--- composeApp/src/commonMain/kotlin/org/noxis/bookpedia/App.kt
+++ composeApp/src/commonMain/kotlin/org/noxis/bookpedia/App.kt
@@ -2,27 +2,18 @@ package org.noxis.bookpedia
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import io.ktor.client.engine.HttpClientEngine
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import org.noxis.bookpedia.book.data.network.KtorRemoteBookDataSource
-import org.noxis.bookpedia.book.data.repository.DefaultBookRepository
+import org.koin.compose.viewmodel.koinViewModel
 import org.noxis.bookpedia.book.presentation.book_list.screen.BookListScreenRoot
 import org.noxis.bookpedia.book.presentation.book_list.viewmodel.BookListViewModel
-import org.noxis.bookpedia.core.data.HttpClientFactory
 
 @Composable
 @Preview
-fun App(engine: HttpClientEngine) {
+fun App() {
+    val viewmodel = koinViewModel<BookListViewModel>()
     MaterialTheme {
         BookListScreenRoot(
-            viewModel = remember { BookListViewModel(
-                bookRepository = DefaultBookRepository(
-                    remoteBookDataSource = KtorRemoteBookDataSource(
-                        httpClient = HttpClientFactory.create(engine)
-                    )
-                )
-            ) },
+            viewModel = viewmodel,
             onBookClick = {
 
             }
---
+++ composeApp/src/commonMain/kotlin/org/noxis/bookpedia/di/Modules.kt
@@ -0,0 +1,26 @@
+package org.noxis.bookpedia.di
+
+import org.koin.core.module.Module
+import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.bind
+import org.koin.dsl.module
+import org.noxis.bookpedia.book.data.network.KtorRemoteBookDataSource
+import org.noxis.bookpedia.book.data.network.RemoteBookDataSource
+import org.noxis.bookpedia.book.domain.BookRepository
+import org.noxis.bookpedia.core.data.HttpClientFactory
+import org.noxis.bookpedia.book.data.repository.DefaultBookRepository
+import org.noxis.bookpedia.book.presentation.book_list.viewmodel.BookListViewModel
+
+expect val platformModule: Module
+
+
+val sharedModule = module {
+
+    single { HttpClientFactory.create(get()) }
+    singleOf(::KtorRemoteBookDataSource).bind<RemoteBookDataSource>() //{ KtorRemoteBookDataSource(get()) }
+    singleOf(::DefaultBookRepository).bind<BookRepository>()
+
+    viewModelOf(::BookListViewModel)
+
+}
