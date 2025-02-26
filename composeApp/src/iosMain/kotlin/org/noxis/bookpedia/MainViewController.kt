2cd00f10935e634bf3e9dba0862996eee9c74fbb
Tumanov Vycheslav <63115274+NoXis82@users.noreply.github.com>
2025-02-26 20:28:52 +0300

    add navigation setup

--- composeApp/src/iosMain/kotlin/org/noxis/bookpedia/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/noxis/bookpedia/MainViewController.kt
@@ -1,6 +1,7 @@
 package org.noxis.bookpedia
 
 import androidx.compose.ui.window.ComposeUIViewController
+import org.noxis.bookpedia.app.App
 import org.noxis.bookpedia.di.initKoin
 
 fun MainViewController() = ComposeUIViewController(
